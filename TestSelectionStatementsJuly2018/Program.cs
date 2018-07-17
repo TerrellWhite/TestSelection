61bc7621c8bcddb2860db33c018cf6b2ffbbde73
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-17 13:59:29 -0400

    Intial Commit

---
+++ TestSelectionStatementsJuly2018/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSelectionStatementsJuly2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            /*Console.Write("Please enter your age:");
+            int input1 = int.Parse(Console.ReadLine());
+            
+            //process
+            if (input1 >= 21)
+            {
+                Console.WriteLine("You can buy Beer!");
+            }
+            else
+            {
+                Console.WriteLine("Ask a homeless guy to do it for you.");
+            }*/
+            //Decide if user is old enough to drive
+            //input
+            Console.Write("Please enter your age:");
+            double input1 = double.Parse(Console.ReadLine());
+
+            if(input1 >= 18)
+            {
+                Console.WriteLine("You can drive!");
+            }
+            else if(input1 >= 16){
+                Console.WriteLine("You can drive but you need someone with a vaild licence next to you!");
+            }
+            else
+            {
+                Console.WriteLine("Ride a bike");
+            }
+        }
+    }
+}
